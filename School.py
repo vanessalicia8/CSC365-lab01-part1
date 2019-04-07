fccf2aa7e0a225324df373bc020cdd2e9862d669
vanessalicia8 <vanessalicia8@gmail.com>
2019-04-06 21:02:50 -0700

    Added all search functions

--- School.py
+++ School.py
@@ -49,12 +49,120 @@ class School:
 		#takes a character representing the student member variable to search
 		#by and a string representing the optional name or number field,
 		#returns a list of all the students matching the search, if the
-		#search yields no results an empty list is returned
+		#search yields no results, an empty line is printed
+		
+		students = []
+		studentCount = 0.0
+		allGPA = 0.0
+
+		if(c == 'A'):
+			for student in self.student_array:
+				if ( student.get_member_var( 'G' ) == aString ):
+					studentCount+= 1
+					allGPA += float(student.GPA)
+			print "\nGrade: " + aString + "\nAverage GPA: " + str(round(allGPA/studentCount, 2)) + "\n"
+			return
+		else:
+			for student in self.student_array:
+				if ( student.get_member_var( c ) == aString ):
+					students.append( student )
+
+		for student in students:
+			if(c == "S"):
+				print "\nStudent: " + student.StLastName + ", " + student.StFirstName + ", " + student.Grade + ", " + student.Classroom + ", " + student.TLastName + ", " + student.TFirstName + "\n"
+			elif(c == "T"):
+				print "\nStudent: " + student.StLastName + ", " + student.StFirstName + "\n"
+			elif(c == "G"):
+				print "\nStudent: " + student.StLastName + ", " + student.StFirstName + "\n"
+			elif(c == "B"):
+				print "\nStudent: " + student.StLastName + ", " + student.StFirstName  + ", " + student.Grade + ", " + student.Classroom + "\n"
+
+		if len(students) == 0:
+			print "\n"
+		return
+
+
+	def search_student_bus( self, aString = None):
+		#takes a character representing the student member variable to search
+		#by and a string representing the optional name or number field,
+		#prints a list of all the students matching the search, if the
+		#search yields no results, an empty line is printed
 		
 		students = []
 
 		for student in self.student_array:
-			if ( student.get_member_var( c ) == aString ):
+			if ( student.get_member_var( 'S' ) == aString ):
 				students.append( student )
 
-		return students
+		for student in students:
+			print "\nStudent: " + student.StLastName + ", " + student.StFirstName + ", " + student.Bus + "\n"
+
+		if len(students) == 0:
+			print "\n"
+		return
+
+	def grade_search( self, aString = None, oString = None ):
+		#takes a string representing the grade number,
+		#prints a list of all the students matching the search, if the
+		#search yields no results, an empty line is printed
+		
+		students = []
+		mystudent = None
+
+		for student in self.student_array:
+			if ( student.get_member_var( 'G' ) == aString ):
+				students.append( student )
+
+		mystudent = students[0]
+		if(oString == "H" or oString == "High"):
+			for student in students:
+				if(student.GPA > mystudent.GPA):
+					mystudent = student
+			
+			print "\n" + mystudent.to_String() + "\n"
+
+		elif(oString == "L" or oString == "Low"):
+			for student in students:
+				if(student.GPA < mystudent.GPA):
+					mystudent = student
+			
+			print "\n" + mystudent.to_String() + "\n"
+
+		if len(students) == 0:
+			print "\n"
+		return
+
+	def grade_info( self ):
+		#computes the total number of students in each grade
+      #prints result
+		
+		kinder = first = second = third = fourth = fifth = sixth = 0
+
+
+		for student in self.student_array:
+			if ( student.get_member_var( 'G' ) == "0" ):
+				kinder += 1
+			elif ( student.get_member_var( 'G' ) == "1" ):
+				first += 1
+			elif ( student.get_member_var( 'G' ) == "2" ):
+				second += 1
+			elif ( student.get_member_var( 'G' ) == "3" ):
+				third += 1
+			elif ( student.get_member_var( 'G' ) == "4" ):
+				fourth += 1
+			elif ( student.get_member_var( 'G' ) == "5" ):
+				fifth += 1
+			elif ( student.get_member_var( 'G' ) == "6" ):
+				sixth += 1
+
+		print "\nGrade 0: " + str(kinder)
+		print "\nGrade 1: " + str(first)
+		print "\nGrade 2: " + str(second)
+		print "\nGrade 3: " + str(third)
+		print "\nGrade 4: " + str(fourth)
+		print "\nGrade 5: " + str(fifth)
+		print "\nGrade 6: " + str(sixth) + "\n"
+		return
+
+
+
