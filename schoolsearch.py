6bc682644114cbbf7d9212faca7cdc67bec1d941
vanessalicia8 <vanessalicia8@gmail.com>
2019-04-06 21:02:28 -0700

    Added all search functions

--- schoolsearch.py
+++ schoolsearch.py
@@ -40,11 +40,26 @@ def main():
 
       command = prompt()
       values = command.split()
+<<<<<<< HEAD
+      #Quit
+=======
       numArgs = len( values )
 
+>>>>>>> 4af2b517e3da6a9a38ce5449cb178bb319ea6cbf
       if ( values[0] == "Q" or values[0] == "Quit" ):
          break
+      #Student
       elif ( values[0] == "S" or values[0] == "Student" ):
+<<<<<<< HEAD
+         if (len(values) == 3):
+            if(values[2] == "B" or values[2] == "Bus"):
+               the_school.search_student_bus(values[1])
+            else:
+               print "\n" + values[2] + " is not a valid command\n"
+         else:
+            the_school.search( 'S', values[1] )
+      #Teacher
+=======
 
          if ( numArgs == 3 ):
             c = handle_options( values[-1] )
@@ -52,17 +67,27 @@ def main():
          students = the_school.search( 'S', values[1] )
          print_info( students )
 
+>>>>>>> 4af2b517e3da6a9a38ce5449cb178bb319ea6cbf
       elif ( values[0] == "T" or values[0] == "Teacher" ):
-         print( the_school.search( 'T', values[1] ) )
-
+         the_school.search( 'T', values[1] )
+      #Bus
       elif ( values[0] == "B" or values[0] == "Bus" ):
-         print( the_school.search( 'B', values[1] ) )
-
+         the_school.search( 'B', values[1] )
+      #Grade
+      elif ( values[0] == "G" or values[0] == "Grade" ):
+         if (len(values) == 3):
+            if(values[2] == "H" or values[2] == "L" or values[2] == "High" or values[2] == "Low"):
+               the_school.grade_search( values[1], values[2] )
+            else:
+               print "\n" + values[2] + " is not a valid command\n"
+         else:
+            the_school.search( 'G', values[1] )
+      #Average
       elif ( values[0] == "A" or values[0] == "Average" ):
-         print( the_school.search( 'A', values[1] ) )
-
+         the_school.search( 'A', values[1] )
+      #Info
       elif ( values[0] == "I" or values[0] == "Info" ):
-         print( search_val )
+         the_school.grade_info()
 
 
 if __name__ == "__main__":
